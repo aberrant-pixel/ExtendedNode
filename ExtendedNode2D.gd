bf0f81bdeca5b74377df3f5a85772c27b115934a
Evan <evantellep@gmail.com>
2022-09-05 16:55:11 -0400

    Created ExtendedNode2D

---
+++ ExtendedNode2D.gd
@@ -0,0 +1,23 @@
+tool
+extends Node2D
+class_name ExtendedNode2D
+
+### DO NOT OVERRIDE THIS ###
+# Override the virtual functions instead
+func _process(delta: float) -> void:
+	if Engine.editor_hint:
+		_editor_process(delta)
+	if not Engine.editor_hint:
+		_game_process(delta)
+	
+	_shared_process(delta)
+
+# virtual functions
+func _editor_process(delta: float) -> void:
+	pass
+
+func _game_process(delta: float) -> void:
+	pass
+
+func _shared_process(delta: float) -> void:
+	pass
