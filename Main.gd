4caf0c7a635522b6a41c69ad88e7f771b83cddcb
Evan <evantellep@gmail.com>
2022-09-05 11:15:11 -0400

    Remove custom node and rename addon

--- Main.gd
+++ Main.gd
@@ -4,10 +4,9 @@ extends EditorPlugin
 
 # Initialize plugin
 func _enter_tree() -> void:
-	add_custom_type("ExtendedNode2D", "ExtendedNode2D", preload("ExtendedNode2D.gd"), preload("icon.png"))
+	pass
 
 
 # Clean up plugin
 func _exit_tree() -> void:
-	remove_custom_type("ExtendedNode2D")
 	pass
