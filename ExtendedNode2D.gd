60550b6db62f5084858b5acbde93c06e32a1f0c3
Evan <evantellep@gmail.com>
2022-10-09 21:57:53 -0400

    More missed godot 4 stuff

--- ExtendedNode2D.gd
+++ ExtendedNode2D.gd
@@ -5,9 +5,9 @@ class_name ExtendedNode2D
 ### DO NOT OVERRIDE THIS ###
 # Override the virtual functions instead
 func _process(delta: float) -> void:
-	if Engine.editor_hint:
+	if Engine.is_editor_hint():
 		_editor_process(delta)
-	if not Engine.editor_hint:
+	else:
 		_game_process(delta)
 	
 	_shared_process(delta)
